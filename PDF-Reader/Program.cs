c354bc0520c152ee20e42d0a0fe9053071719c27
Javier Astudillo <javier_astudillo@live.com.mx>
2019-04-02 02:40:03 -0600

    Added arguments to console and debug info

--- PDF-Reader/Program.cs
+++ PDF-Reader/Program.cs
@@ -9,15 +9,33 @@ namespace PDF_Reader
         {
             Console.WriteLine("Facturas !!!");
 
+            string fileNameInput = "factura.txt", fileNameOutput = "facturaSalida.txt";
+
+            Console.WriteLine(args.Count());
+
+            if (args.Count() > 0)
+            {
+                fileNameInput = args[0];
+            }
+            if(args.Count() > 1)
+            {
+                fileNameOutput = args[1];
+            }
+
+            Console.WriteLine($"Archivo entrada: {fileNameInput}");
+            Console.WriteLine($"Archivo salida: {fileNameOutput}");
+
             var reader = new FacturaReader();
 
-            var facturas = reader.LeerFacturas("factura.txt");
+            var facturas = reader.LeerFacturas(fileNameInput);
 
             facturas = facturas.OrderBy(f => f.EfectoComprobante)
                 .ThenBy(f => f.Estatus)
                 .ThenBy(f => f.FechaEmision);
 
-            var exito = reader.EscribirFacturasACsv(facturas, "facturasFormat.txt");
+            var exito = reader.EscribirFacturasACsv(facturas, fileNameOutput);
+
+            Console.WriteLine("Factura generada!!!");
         }
     }
 }
